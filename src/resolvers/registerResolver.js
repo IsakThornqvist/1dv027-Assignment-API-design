bc087d17c4ad2c6d3a0e2b548ab2258e23cc0761
Isak <it222hp@student.lnu.se>
2026-03-04 12:56:33 +0100

    prettier formatting

--- src/resolvers/registerResolver.js
+++ src/resolvers/registerResolver.js
@@ -3,50 +3,48 @@ import bcrypt from "bcryptjs"
 import jwt from "jsonwebtoken"
 
 export const registerResolver = {
-
   Mutation: {
-    register : async(_, { email, password}) => {
-        if (!password || !email) {
-            throw new Error('Email and password are needed!')
-        }
-        if (password.length < 5) {
-            throw new Error('Password need to be longer than 5 characters!')
-        } 
+    register: async (_, { email, password }) => {
+      if (!password || !email) {
+        throw new Error("Email and password are needed!")
+      }
+      if (password.length < 5) {
+        throw new Error("Password need to be longer than 5 characters!")
+      }
 
-        const emailAlreadyExists = await prisma.user.findUnique({
-            where: { 
-                email 
-            },
-        })
-
-        if (emailAlreadyExists) {
-            throw new Error('User already exists in database!')
-        }
-
-        const passwordHashed = await bcrypt.hash(password, 10)
-
-        const user = await prisma.user.create({
-            data: {
-                email,
-                password: passwordHashed,
-            },
-        })
-
-        const jwtToken = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
-            expiresIn: '7d'
-        })
-
-        return {
-            token: jwtToken,
-            user: user,
-            }
-        }
-    },
-
-
-    Query: {
-        allUsers: async() => {
-            return await prisma.user.findMany()
+      const emailAlreadyExists = await prisma.user.findUnique({
+        where: {
+          email,
         },
-    }
+      })
+
+      if (emailAlreadyExists) {
+        throw new Error("User already exists in database!")
+      }
+
+      const passwordHashed = await bcrypt.hash(password, 10)
+
+      const user = await prisma.user.create({
+        data: {
+          email,
+          password: passwordHashed,
+        },
+      })
+
+      const jwtToken = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+      })
+
+      return {
+        token: jwtToken,
+        user: user,
+      }
+    },
+  },
+
+  Query: {
+    allUsers: async () => {
+      return await prisma.user.findMany()
+    },
+  },
 }
